17853e5d3c38821631168c161d72246be719602e
ketrin996 <bloomingkate789@gmail.com>
2019-03-06 09:39:58 +0200

    Task with arrays methods

--- index.js
+++ index.js
@@ -1,12 +1,97 @@
-var a = [1, 2, 3, 4];
+var arr = [1,2,3,4,5,6];
+var matrix = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 
-var b = a.filter(function (item, index) {
-    return item % 2 === 0
-})
-console.log(b);
-a.sort(function (a,b) {
-return b-a
-})
-console.log(a);
-;
+function incForeach(array) {
+    console.log("ForEach method:");
+    array.forEach(function (item) {
+        console.log(item + 1);
+    });
+
+}
+
+function incFor(array) {
+    console.log("For method:");
+    for(var i = 0 ; i < array.length; i++){
+        array[i] = array[i]+1;
+    }
+   return array;
+}
+
+function incMap(array) {
+    console.log("Map method");
+    var mapInc = array.map(function(item) {
+        return item + 1;
+    });
+    return mapInc;
+}
+
+function avgForeach(array) {
+    console.log("ForEach avg method:");
+    var sum = 0;
+    array.forEach(function (item) {
+        sum += item;
+    });
+    return sum/(array.length+1);
+}
+
+function avgFor(array) {
+    console.log("For avg method:");
+    var sum = 0;
+    for(var i = 0; i < array.length; i++)
+    {
+        sum += array[i];
+    }
+    return sum/(array.length+1);
+}
+
+function ForEvenIndex(array){
+    console.log("For Even Index Sum");
+    var sum = 0;
+    for(var i=0; i< array.length; i++){
+        if(i % 2 !== 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+function ForEachEvenIndex(array){
+    console.log("ForEach Even Index Sum");
+    var sum = 0;
+    array.forEach(function (item,i) {
+        if (i % 2 !== 0) {
+            sum += item;
+        }
+    });
+    return sum;
+}
+
+function CountMatrixDiagonal(matrix){
+    console.log("Matrix Count:");
+    var sumMain=0;
+    var sumReverse=0;
+    for (var i = 0; i < matrix.length; i++){
+        sumMain += matrix[i][i];
+    }
+    for (var i = 0; i < matrix.length; i++) {
+        sumReverse += matrix[i][matrix.length - 1 - i];
+    }
+    console.log(sumMain-sumReverse);
+}
+
+console.log(ForEvenIndex(arr));
+console.log(ForEachEvenIndex(arr));
+
+console.log(avgForeach(arr));
+console.log(avgFor(arr));
+
+console.log(incMap(arr));
+incForeach(arr);
+console.log(incFor(arr));
+
+CountMatrixDiagonal(matrix);
+
 
