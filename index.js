c7e88feff2dccac5315c2a9d574f08721d073b76
ketrin996 <bloomingkate789@gmail.com>
2019-03-11 22:22:46 +0200

    Object functions

--- index.js
+++ index.js
@@ -1,25 +1,80 @@
-var str = "abc";
-
-function reverseString(string) {
-    var resStr="";
-    for (var i = string.length-1; i >= 0; i--) {
-         resStr+=string[i];
+var sum = function(a){
+    return (b) => {
+        return a + b;
     }
-    return resStr;
-}
+};
 
-function reverseStringMethods(string){
-    return string.split("").reverse().join("");
-}
+function sumN(a) {
 
-function reverseRecurtion(string){
-    if(string==="")
-        return "";
-    else{
-    return reverseRecurtion(string.substr(1)) + string.charAt(0);
+    var currentSum = a;
+
+    function f(b) {
+        if (b !== undefined) {
+            currentSum += b;
+            return f;
+        }
+        else
+        {
+            return currentSum;
+        }
     }
-}
 
-console.log(reverseString(str));
-console.log(reverseStringMethods(str));
-console.log(reverseRecurtion(str));
+    f.toString = function () {
+        return currentSum;
+    };
+    return f;
+}
+   // console.log(sumN(1)(2)(3)(5));
+
+    var todoData = [
+        {
+            title: 'todoBlock1',
+            todos: [{
+                text: 'todotext',
+                completed: false,
+            }]
+        },
+        {
+            title: 'todoBlock2',
+            todos: [{
+                text: 'todotext',
+                completed: false,
+            },
+                {
+                    text: 'todotext2',
+                    completed: false,
+                }]
+        },
+    ];
+
+    function addTodo(boardList, groupIndex, taskText) {
+        boardList[groupIndex].todos.push(
+            {
+                text: taskText,
+                compleated: false
+            });
+    }
+
+    function addTodoBoard(boardList, newBoard) {
+        boardList.push(newBoard);
+    }
+
+    function deleteBoard(boardList, groupIndex) {
+        delete boardList[groupIndex];
+    }
+
+    function deleteTodo(boardList, groupIndex, taskIndex) {
+        delete boardList[groupIndex].todos[taskIndex];
+    }
+
+    function changeTaskStatus(boardList, groupIndex, taskIndex) {
+        let status = boardList[groupIndex].todos[taskIndex].completed;
+        (status === false) ? boardList[groupIndex].todos[taskIndex].completed = true :
+            boardList[groupIndex].todos[taskIndex].completed = false;
+    }
+//deleteBoard(todoData, 0);
+//addTodo(todoData, 0, 'dosomething');
+//addTodoBoard(todoData, board);
+//deleteTodo(todoData, 1, 1);
+//changeTaskStatus(todoData, 1, 0);
+
